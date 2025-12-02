b5cfdaffe42c377419435df93c0b2cf009928614
Tamara Ancheva <tamaraancheva7@gmail.com>
2025-12-01 19:30:47 -0800

    H_Task_1

---
+++ github-repository-hw/Home_work/src/main/java/java_hw_1/h/H/H_Task_1.java
@@ -0,0 +1,14 @@
+package java_hw_1.h.H;
+
+public class H_Task_1 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b =9;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
