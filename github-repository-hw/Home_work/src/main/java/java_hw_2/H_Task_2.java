ff62a36ac34f8a6ae6e8b67f59217bd0e6fa9868
tamaraancheva <tamaraancheva7@gmail.com>
2025-12-05 21:49:22 -0800

    Fix: corrected Task 2 logic

--- github-repository-hw/Home_work/src/main/java/java_hw_2/H_Task_2.java
+++ github-repository-hw/Home_work/src/main/java/java_hw_2/H_Task_2.java
@@ -2,25 +2,6 @@ package java_hw_2;
 
 public class H_Task_2 {
     public static void main(String[] args) {
-    String string1 = "This line that i want to cut, cause it is too long";
-    String string2 = string1.substring(0, 35);
-    String string3 = string2.replace("want to cut","dont want to cut") + " it is perfect";
-
-    System.out.println(string1);
-    System.out.println(string1.length());
-    System.out.println(string2);
-    System.out.println(string2.length());
-    System.out.println(string3);
-    System.out.println(string3.length());
-
-
-    int from = string1.indexOf("This");
-    System.out.println(from);
-    int to = string1.indexOf("cause") + "cause".length();
-    System.out.println(to);
-
-    System.out.println(string1.substring(0, 35));
-
         String text = "Testing is my favourite job";
         System.out.println(text);
         String[] words = text.split(" ");
@@ -43,14 +24,6 @@ public class H_Task_2 {
                 && words[0].length() > words[2].length()
                 && words[0].length() > words[3].length()
                 && words[0].length() > words[4].length());
-
-
-        String sentence = "Completely random text in English. In it, we just need to determine how man times the character 'a' occurs there. And we can use the split method and the length method.";
-        sentence = sentence.toLowerCase();;
-        int count = sentence.split("a").length - 1;
-        System.out.println(count);
-
-
     }
 }
 
