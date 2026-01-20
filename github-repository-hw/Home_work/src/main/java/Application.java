a76c5e5911a1b8f817e9afc288dd89487bba629b
tamaraancheva <tamaraancheva7@gmail.com>
2026-01-19 17:00:49 -0800

    HW5: tasks completed

---
+++ github-repository-hw/Home_work/src/main/java/Application.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+
+        NumbersApplication game = new NumbersApplication(name);
+        game.startGame(scanner);
+
+        scanner.close();
+    }
+}
